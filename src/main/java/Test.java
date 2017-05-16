63b32331e5bd2e03d3e0ce2bdaec2f0183a452a1
jiangxingqi <jxq0816@126.com>
2017-05-16 20:13:03 +0800

    log4j 配置

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -12,7 +12,7 @@ public class Test {
     public static void main(String[] args) {
 
         logger.debug("This is debug message.");
-        logger.info("This is info message.");
-        logger.error("This is error message.");
+        //logger.info("This is info message.");
+        //logger.error("This is error message.");
     }
 }
